5c4f3e27a9ea36a94412e2ba6170aef7dcd0a115
Zhengyang Duan <zyduan522@gmail.com>
2018-06-08 10:40:04 -0400

    init the app

---
+++ ToDoList/ToDoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Zhengyang Duan on 2018-06-08.
+//  Copyright © 2018 Zhengyang Duan. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+    
+    let item = ["go to supermarket", "go to school", "go to bank"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK: -declare table rows and data source
+    //rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    //datasource
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Todo")
+        cell.textLabel?.text = item[indexPath.row]
+        return cell
+        
+    }
+
+}
+
